f09c6ffde5761272d106ad4bdfe6a80ff40e5cac
NIKHIL MATTA <67046318+nikhil478@users.noreply.github.com>
2023-08-28 05:09:23 +0530

    boiler code of drop test (WIP)

--- test/drop.test.js
+++ test/drop.test.js
@@ -1,27 +1,50 @@
-// const RelysiaSDK = require('../src/relysia-sdk');
+const {expect} = require('chai');
+const Validator = require('../src/Pay/validator.js');
+const Joi = require('joi');
 
-// describe('/v1/drop endpoint test', async function() {
-//   this.timeout(10000);
-//   const secretKey = '';
-//   const privateKey = '';
-//   const relysia = new RelysiaSDK();
+describe('Validator', () => {
+  describe('txMultipayc', () => {
+    it('should validate options for a multiple payment channel transaction', async () => {
+      const validator = new Validator();
+      const validOptions = {
+        walletID: 'wallet123',
+        input: [
+          {sequenceNum: 1, utxoId: 123},
+          {sequenceNum: 2, utxoId: 456},
+        ],
+        lockTime: '2022-01-01T12:00:00Z',
+        outputs: [
+          {amount: 100, asm: 'OP_2 OP_2 OP_ADD OP_EQUAL'},
+          {amount: 200, asm: 'OP_1 OP_2 OP_ADD OP_EQUAL'},
+        ],
+        changeAddress: 'change123',
+      };
+      try {
+        await validator.txMultipayc(validOptions);
+      } catch (error) {
+        // If validation fails, an exception will be thrown
+        expect.fail('Validation should not fail');
+      }
+    });
 
-//   it('Should successfully send utxo or token with drop endpoint', async () => {
-//     const opts = {
-//       secretKey,
-//       privateKey,
-//       data: {
-//         'dataArray': [
-//           {
-//             'to': '17pd22FKBj9ujwLSjFekkaGGLxfC2bWMAZ',
-//             'amount': 0.00001,
-//             'type': 'BSV',
-//             'notes': 'paying for beer',
-//             'tokenId': 'string',
-//             'sn': 0,
-//           },
-//         ],
-//       },
-//     };
-//   });
-// });
+    it('should throw an error for invalid options', async () => {
+      const validator = new Validator(); // Create an instance of the Validator class
+
+      // Example invalid options (missing required fields)
+      const invalidOptions = {
+        input: [
+          {sequenceNum: 1, utxoId: 123},
+        ],
+        outputs: [
+          {amount: 100, asm: 'OP_2 OP_2 OP_ADD OP_EQUAL'},
+        ],
+      };
+      try {
+        await validator.txMultipayc(invalidOptions);
+        expect.fail('Validation should fail');
+      } catch (error) {
+        expect(error).to.be.an.instanceOf(Joi.ValidationError);
+      }
+    });
+  });
+});
