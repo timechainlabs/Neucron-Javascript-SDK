363b677b9f0ec15b26b96ef2b9251a3a1a888b92
Nikhil Matta <nikhilmatta45@gmail.com>
2024-01-15 12:59:12 +0000

    tested wallet module and added remaining endpoints

--- src/wallet/index.js
+++ src/wallet/index.js
@@ -1,5 +1,5 @@
 import Request from '../request.js';
-import validator from '../wallet/validator.js';
+import validator from './validator.js';
 
 class Wallet {
 
@@ -15,74 +15,71 @@ class Wallet {
 	}
   }
 
-  //TODO: Implement these endpoints: { xPubKeys, setDefaultWallet, getMnemonic}
-
 /**
- * Creates a new wallet for a user.
+ * Creates a new wallet with the specified options.
  * @param {Object} options - The options for wallet creation.
  * @param {string} options.walletName - The name of the wallet to be created.
  * @param {string} [options.mnemonic] - The mnemonic to be used for wallet creation (optional).
+ * @param {string} [options.appId] - The app ID to be used for wallet creation (optional).
  * @throws {Error} Throws an error if the wallet creation request fails.
  * @return {string} The wallet ID if creation is successful.
  */
 async createWallet(options) {
-	try {
-	  // Validate the SDK state
-	  await this.validate();
-  
-	  // Ensure the walletName is provided
-	  if (!options.walletName) {
-		throw new Error('Wallet name is required for wallet creation.');
-	  }
-  
-	  // Define the API endpoint
-	  const endpoint = '/wallet/create';
-  
-	  // Prepare the request body
-	  const requestBody = {};
-  
-	  // Adjust requestHeaders to include walletName in query parameters
-	  const requestHeaders = {
-		Authorization: this.auth.getAuthToken(),
-	  };
-  
-	  // Append walletName to the query parameters
-	  const queryString = `walletName=${encodeURIComponent(options.walletName)}`;
-	  const requestUrl = `${endpoint}?${queryString}`;
-  
-	  // Make the POST request to create the wallet
-	  const response = await this.request.postRequest(requestUrl, requestBody, requestHeaders);
-  
-	  // Handle errors, if any
-	  if (response instanceof Error) {
-		throw response;
-	  }
-  
-	  // Return the wallet ID from the response data
-	  return await response;
-	} catch (error) {
-	  throw new Error('Wallet creation failed: ' + error);
-	}
-  }
-  
+    try {
+
+		this.validate.createWallet(options);
+        await this.validate();
+
+        const endpoint = '/wallet/create';
+
+        const requestBody = {};
+
+        const requestHeaders = {
+            Authorization: this.auth.getAuthToken(),
+			accept: 'application/json'
+        };
+
+        const queryString = `walletName=${encodeURIComponent(options.walletName)}` +
+                            (options.mnemonic ? `&mnemonic=${encodeURIComponent(options.mnemonic)}` : '') +
+                            (options.appId ? `&appID=${encodeURIComponent(options.appId)}` : '');
+        const requestUrl = `${endpoint}?${queryString}`;
+        const response = await this.request.postRequest(requestUrl, requestBody, requestHeaders);
+
+        if (response instanceof Error) {
+            throw response;
+        }
+
+        return response
+    } catch (error) {
+        throw new Error('Wallet creation failed: ' + error);
+    }
+}
 
   /**
    * Lets a user to set default wallet
-   * @param {string} [query.walletId] - using this mnemonic user can create an wallet (optional).
+   * @param {string} options.walletId - wallet Id which need to be set default.
    * @throws {Error} Throws an error if the transaction request fails.
    * @return {Object} The headers of the response if successful.
    */
-  async setDefaultWallet(query) {
+  async setDefaultWallet(options) {
 
-	// TODO: R&D wrong config need to fix from neucron team
 	try {
+	  this.validate.setDefaultWallet(options);
 	  await this.validate();
 
 	  const endpoint = '/wallet/default';
 
 	  const requestBody = {};
 
-	  const response = await this.request.postRequest(endpoint, requestBody, query);
+	  const requestHeaders = {
+		Authorization: this.auth.getAuthToken(),
+		accept: 'application/json'
+	};
+
+	  const queryString = `walletID=${encodeURIComponent(options.walletId)}`
+	  const requestUrl = `${endpoint}?${queryString}`;
+
+	  const response = await this.request.postRequest(requestUrl, requestBody, requestHeaders);
 
 	  if (response instanceof Error) {
 		throw response;
@@ -110,7 +107,6 @@ async createWallet(options) {
 	  };
 
 	  if (options && options.walletId){
-
 		endpoint += `?walletID=${options.walletId}`;
 	  }
 
@@ -304,6 +300,68 @@ async createWallet(options) {
 		throw response;
 	  }
 	  return response.data.list;
+	} catch (error) {
+	  throw new Error('Unable to fetch keys : ' + error);
+	}
+  }
+
+  /**
+   * return list of utxos
+   * @param {string} [options.walletId] - walletId of the user he want to run options on if not present then all utxos will return of user.
+   * @throws {Error} Throws an error if the transaction request fails.
+   * @return {Object} The headers of the response if successful.
+   */
+  async getXPubKeys(options) {
+	try {
+	  await this.validate();
+
+	  let endpoint = '/wallet/xpubkeys';
+
+	  const requestHeaders = {
+		Authorization: this.auth.getAuthToken()
+	  };
+
+	  if (options && options.walletId){
+		endpoint += `?walletID=${options.walletId}`;
+	  }
+
+	  const response = await this.request.getRequest(endpoint, requestHeaders);
+
+	  if (response instanceof Error) {
+		throw response;
+	  }
+	  return response;
+	} catch (error) {
+	  throw new Error('Unable to fetch keys : ' + error);
+	}
+  }
+
+  /**
+   * return list of utxos
+   * @param {string} [options.walletId] - walletId of the user he want to run options on if not present then all utxos will return of user.
+   * @throws {Error} Throws an error if the transaction request fails.
+   * @return {Object} The headers of the response if successful.
+   */
+  async getMnemonic(options) {
+	try {
+	  await this.validate();
+
+	  let endpoint = '/wallet/mnemonic';
+
+	  const requestHeaders = {
+		Authorization: this.auth.getAuthToken()
+	  };
+
+	  if (options && options.walletId){
+		endpoint += `?walletID=${options.walletId}`;
+	  }
+
+	  const response = await this.request.getRequest(endpoint, requestHeaders);
+
+	  if (response instanceof Error) {
+		throw response;
+	  }
+	  return response;
 	} catch (error) {
 	  throw new Error('Unable to fetch keys : ' + error);
 	}
